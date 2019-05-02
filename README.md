8fbf0252414487eba1807f6805e56c5e80af1cbb
Garrett Chen <g.chen4@share.epsb.ca>
2019-05-02 11:48:54 -0600

    move and readme added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Backpack
 
 Mini Multi Day
+
+## Abstraction 
+A container such as a backpack has many qualities, such as size, volume, color, brand, etc. but the program only requires the contents of the backpack and its name. 
+## Aggregation
+All of the items that are put inside the containers are an example of Aggregation.
+## Encapsulation
+The getter and setter methods in the holding class (getItems, getName) are examples of encapsulation, along with the str and repr magic methods for the items,
+## Polymorphism
+All the items each have a "Name"(Subject or Color). Though they all are an item (such as a pen), all the pens have different colors.
--- backpack.py
+++ backpack.py
@@ -1,148 +1,228 @@
 '''
 title: backpack multiday project
-author: garrett
+author: Garrett Chen
 date created: 2019-04-02
 '''
 
-class locker:
+class holding:
     def __init__(self, name):
         self.name = name
         self.contents = []
-    def addItem(self):
-        self.contents.append(input('new item > '))
-    def moveItem(self):
-        pass
-    def getName(self):
-        return self.name
-    def getContents(self):
-        print('%s: %s' %(self.name, self.contents))
 
-class backpack:
-    def __init__(self, name):
-        self.name = name
-        self.contents = []
-    def addItem(self):
-        self.contents.append(input('new item > '))
-    def moveItem(self):
-        pass
-    def getName(self):
-        return self.name
-    def getContents(self):
-        print('%s: %s' %(self.name, self.contents))
+    def addItem(self, obj):
+        self.contents.append(obj)
 
-class pencilcase:
-    def __init__(self, name):
-        self.name = name
-        self.contents = []
-    def addItem(self):
-        self.contents.append(input('new item > '))
-    def moveItem(self):
-        pass
-    def getName(self):
-        return self.name
-    def getContents(self):
-        print('%s: %s' %(self.name, self.contents))
+    def getItems(self):
+        return self.contents
 
-
-class items:
-    def __init__(self):
-        self.name = " "
-
-    def addItem(self):
-        self.name = input('add an item')
+    def removeItems(self, num):
+        self.contents.pop(num)
 
     def getName(self):
         return self.name
-
-def menu():
-
-    print(locker.getName())
-    print('-----')
-    for i in range(len(locker.contents)):
-        print(locker.contents[i])
-    print(' ')
-    print(backpack.getName())
-    print('-----')
-    for i in range(len(backpack.contents)):
-        print(backpack.contents[i])
-    print(' ')
-    print(pencilCase.getName())
-    print('-----')
-    for i in range(len(pencilCase.contents)):
-        print(pencilCase.contents[i])
-    print(' ')
-    print('''
-1) add an item
-2) move an item
-    ''')
-    action = input('> ')
-    if action == '1':
-        newItem = items()
-        newItem.addItem()
-        allItems.append(newItem.getName())
-        locker.contents.append(newItem.getName())
-    if action == '2':
-        count = -1
-        for i in range(len(allItems)):
-            if isinstance(allItems[i], list):
-                for j in range(len(allItems[i])):
-                    count += 1
-                    print(str(count) + ": " + str(allItems[i][j]))
-            else:
-                count += 1
-                print(str(count) + ": " + str(allItems[i]))
-
-        itemchoice = int(input('what item number? '))
-        if isinstance(allItems[itemchoice], list):
-        print(allItems[itemchoice])
-        print('move where?')
-
-
-
-allItems = []
-locker = locker('locker')
-locker.contents = ['binders', 'jacket']
-backpack = backpack('backpack')
-backpack.contents = ['pencil case', 'laptop']
-pencilCase = pencilcase('pencil case')
-pencilCase.contents = ['pencil', 'pen']
-allItems.append(locker.contents)
-allItems.append(backpack.contents)
-allItems.append(pencilCase.contents)
-
-while True:
-    menu()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class binder:
-    def __init__(self, name):
-        self.name = name
-
-
-class electronics:
-    def __init__(self, name):
-        self.name = name
-
 
 class notebook:
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return ('%s notebook'%(self.name))
+
+    def __repr__(self):
+        return ('%s notebook' % (self.name))
+
+class pens:
+    def __init__(self, color):
+        self.color = color
+
+    def __str__(self):
+        return ('%s pen'%(self.color))
+
+    def __repr__(self):
+        return ('%s pen' % (self.color))
+
+class binder:
+    def __init__(self, subject):
+        self.subject = subject
+
+    def __str__(self):
+        return ('%s binder'%(self.subject))
+
+    def __repr__(self):
+        return ('%s binder' % (self.subject))
 
 class highlighter:
+    def __init__(self, color):
+        self.color = color
+
+    def __str__(self):
+        return ('%s highlighter'%(self.color))
+
+    def __repr__(self):
+        return ('%s highlighter' % (self.color))
+
+class misc:
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+def menu():
+    #Prints the names of the containers and their contents
+    print(locker.name)
+    print('-----')
+    for i in range(len(locker.getItems())):
+        print(locker.getItems()[i])
+    print('')
+    print(bag.name)
+    print('-----')
+    for i in range(len(bag.getItems())):
+        print(bag.getItems()[i])
+    print('')
+    print(pencilCase.name)
+    print('-----')
+    for i in range(len(pencilCase.getItems())):
+        print(pencilCase.getItems()[i])
+    print('')
+    print('''
+Options:    
+1) Add an item
+2) Move an item
+    ''')
+    choice = int(input('> ')) # User input choice of action
+
+## Adding Items section
+    if choice == 1:
+        print('''
+Add what?
+1) notebook
+2) pen
+3) binder
+4) highlighter
+5) other
+''')
+        item = int(input('>'))
+        if item in (1, 3): # Gets options from dictionary below
+            return locker.addItem(addOptions[item](input('Subject: ')))
+        elif item in (2, 4):
+            return locker.addItem(addOptions[item](input('Color: ')))
+        elif item == 5:
+            return locker.addItem(addOptions[item](input('Name: ')))
+        else:
+            print('invalid item')
+
+### Moving items section
+    elif choice == 2:
+        temp = locker.getItems() + bag.getItems() + pencilCase.getItems() # Collects all the items from the three containers
+        holdingitem = None # The item being held temporarily
+        for i in range(len(temp)):
+            print('%s: %s'%(i, temp[i]))
+        numb = int(input('number '))
+
+        ## IF ITEM IS IN LOCKER
+        if temp[numb] in locker.getItems():
+            print('%s is in %s'%(temp[numb], locker.getName()))
+            print('''
+To where?
+1) Bag
+2) Pencil Case
+            ''')
+            moving = int(input('> '))
+
+            # Moving from Locker to Bag
+            if moving == 1:
+                for i in range(len(locker.getItems())):
+                    if temp[numb] == locker.getItems()[i-1]:
+                        holdingitem = locker.getItems()[i-1]
+                        locker.removeItems(i-1)
+                        bag.addItem(holdingitem)
+
+            # Moving from Locker to Pencil Case
+            elif moving == 2:
+                for i in range(len(locker.getItems())):
+                    if temp[numb] == locker.getItems()[i-1]:
+                        holdingitem = locker.getItems()[i-1]
+                        locker.removeItems(i-1)
+                        pencilCase.addItem(holdingitem)
+
+        ## IF ITEM IS IN BAG
+        elif temp[numb] in bag.getItems():
+            print('%s is in %s'%(temp[numb], bag.getName()))
+            print('''
+To where?
+1) Locker
+2) Pencil Case
+                                    ''')
+            moving = int(input('> '))
+
+            # Moving from Bag to Locker
+            if moving == 1:
+                for i in range(len(bag.getItems())):
+                    if temp[numb] == bag.getItems()[i-1]:
+                        holdingitem = bag.getItems()[i-1]
+                        bag.removeItems(i-1)
+                        locker.addItem(holdingitem)
+
+            # Moving from Bag to Pencil Case
+            elif moving == 2:
+                for i in range(len(bag.getItems())):
+                    if temp[numb] == bag.getItems()[i-1]:
+                        holdingitem = bag.getItems()[i-1]
+                        bag.removeItems(i-1)
+                        pencilCase.addItem(holdingitem)
+
+        ## IF ITEM IS IN PENCIL CASE
+        elif temp[numb] in pencilCase.getItems():
+            print('%s is in %s'%(temp[numb], pencilCase.getName()))
+            print('''
+To where?
+1) Locker
+2) Bag
+                        ''')
+            moving = int(input('> '))
+
+            # Moving from Pencil Case to Locker
+            if moving == 1:
+                for i in range(len(pencilCase.getItems())):
+                    if temp[numb] == pencilCase.getItems()[i-1]:
+                        holdingitem = pencilCase.getItems()[i-1]
+                        pencilCase.removeItems(i-1)
+                        locker.addItem(holdingitem)
+
+            # Moving from Pencil Case to Bag
+            elif moving == 2:
+                for i in range(len(pencilCase.getItems())):
+                    if temp[numb] == pencilCase.getItems()[i-1]:
+                        holdingitem = pencilCase.getItems()[i-1]
+                        pencilCase.removeItems(i-1)
+                        bag.addItem(holdingitem)
+
+
+### CODE STARTS HERE
+# Creates the container objects
+locker = holding('Locker')
+bag = holding('Bag')
+pencilCase = holding('Pencil case')
+
+# Dictionary is used for adding items
+addOptions = {
+     1 : notebook,
+     2 : pens,
+     3 : binder,
+     4 : highlighter,
+     5 : misc
+}
+
+# Preset items
+locker.addItem(pens('blue'))
+locker.addItem(binder('math'))
+bag.addItem(notebook('english'))
+bag.addItem(pens('red'))
+pencilCase.addItem(misc('ruler'))
+pencilCase.addItem(pens('green'))
+while True:
+    menu()
